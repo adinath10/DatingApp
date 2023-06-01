ea7e97c07e399012393f58338589fc85e20f4fd2
adinath10 <a1kkankekar@gmail.com>
2023-06-01 13:47:35 +0530

    End Of Section 14

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { MembersService } from '../_services/members.service';
+import { Pagination } from '../_models/pagination';
 
 @Component({
   selector: 'app-lists',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListsComponent implements OnInit {
 
-  constructor() { }
+  // Partial type is useful when working with APIs that return objects with a lot of properties, 
+  // but not all of them are required.
+
+  // When updating an object, we provide some of the properties. 
+  // The Partial type makes it easy to create a type that represents the updated object, 
+  // while still keeping the original type intact.
+  members ?: Partial<Member[]>;
+  predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
+  pagination ?: Pagination;
+
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadLikes()
+  }
+
+  loadLikes(){
+    this.memberService.getLikes(this.predicate ,this.pageNumber, this.pageSize).subscribe(response => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    })
+  }
+
+  pageChanged(event: any){
+    this.pageNumber = event.page;
+    this.loadLikes();
   }
 
 }
